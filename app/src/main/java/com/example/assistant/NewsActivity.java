fdfe6e1d7d6b86f344b3c09a13b3560239620e56
ruibl <15512505699@163.com>
2020-12-13 12:24:53 +0800

    2020/12/13 网页渲染问题

--- app/src/main/java/com/example/assistant/NewsActivity.java
+++ app/src/main/java/com/example/assistant/NewsActivity.java
@@ -3,6 +3,7 @@ package com.example.assistant;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 
@@ -15,7 +16,26 @@ public class NewsActivity extends AppCompatActivity {
         setContentView(R.layout.activity_news);
         N_web = findViewById(R.id.N_web);
         N_web.loadUrl("http://xcbnew.stdu.edu.cn/");
-        N_web.getSettings().setJavaScriptEnabled(true);
+        //声明WebSettings子类
+        WebSettings webSettings = N_web.getSettings();
+        //如果访问的页面中要与Javascript交互，则webview必须设置支持Javascript
+        webSettings.setJavaScriptEnabled(true);
+        // 若加载的 html 里有JS 在执行动画等操作，会造成资源浪费（CPU、电量）
+        // 在 onStop 和 onResume 里分别把 setJavaScriptEnabled() 给设置成 false 和 true 即可
+        //设置自适应屏幕，两者合用
+        webSettings.setUseWideViewPort(true); //将图片调整到适合webview的大小
+        webSettings.setLoadWithOverviewMode(true); // 缩放至屏幕的大小
+        //缩放操作
+        webSettings.setSupportZoom(true); //支持缩放，默认为true。是下面那个的前提。
+        webSettings.setBuiltInZoomControls(true); //设置内置的缩放控件。若为false，则该WebView不可缩放
+        webSettings.setDisplayZoomControls(false); //隐藏原生的缩放控件
+        //其他细节操作
+        webSettings.setCacheMode(WebSettings.LOAD_CACHE_ELSE_NETWORK); //关闭webview中缓存
+        webSettings.setAllowFileAccess(true); //设置可以访问文件
+        webSettings.setJavaScriptCanOpenWindowsAutomatically(true); //支持通过JS打开新窗口
+        webSettings.setLoadsImagesAutomatically(true); //支持自动加载图片
+        webSettings.setDefaultTextEncodingName("utf-8");//设置编码格式
+        //N_web.getSettings().setJavaScriptEnabled(true);
         N_web.setWebViewClient(new WebViewClient(){
             @Override
             public boolean shouldOverrideUrlLoading(WebView view, String url) {
